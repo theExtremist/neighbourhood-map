2d9e8b3c0d501ceb6f3fe96b466c4288aca36594
Thierry Michel <Thierry Michel>
2016-12-07 18:21:23 +1100

    Add placeholder locations Add overlay drawer menu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,35 @@
+function toggleNav() {
+  document.getElementById("myNav").classList.toggle("open");
+}
+
+var initialPlaces = [
+            {
+                "name"      : "Station",
+                "location"  : {lat: -37.8726662, lng: 145.1232088,}
+            },
+
+            {
+                "name"      : "YOMG",
+                "location"  : {lat: -37.8734595, lng: 145.091393}
+            },
+];
 
 
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+var place = function(data) {
+    this.name = ko.observable(data.name);
+    this.location = ko.observable(data.location);
+}
+
+
+var ViewModel = function() {
+    var self = this;
+
+    this.locations = ko.observableArray([]);
+
+       initialPlaces.forEach(function (p) {
+       self.locations.push(new place(p))
     });
 }
+
+
+ko.applyBindings(new ViewModel());
---
+++ js/map.js
@@ -0,0 +1,26 @@
+var map;
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+                              center: {lat: -37.882, lng: 145.163}, zoom: 13,
+                              mapTypeControl: false
+    });
+
+
+    google.maps.event.addListener(map, 'click', function(event) {
+                  addMarker(event.latLng, map);
+                });
+}
+
+function addMarker(location, map) {
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
+    var marker = new google.maps.Marker({
+        position: location,
+        label: "X",
+        map: map
+    });
+}
+
+
